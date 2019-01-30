bcf10c2ab73f832aa1f2e461e6b81c328dce3cfc
Arthur Rump <hello@arthurrump.com>
2019-01-30 21:04:13 +0100

    Add CLI for easier testing

---
+++ src/NetCore.Versions.Checks.Cli/Program.fs
@@ -0,0 +1,8 @@
+﻿module NetCore.Versions.Checks.Cli
+
+[<EntryPoint>]
+let main argv =
+    match argv with
+    | [| owner; repo; hash |] -> Run.runLocal owner repo hash |> Async.RunSynchronously
+    | _ -> printfn "Arguments: owner repo hash"
+    0 
